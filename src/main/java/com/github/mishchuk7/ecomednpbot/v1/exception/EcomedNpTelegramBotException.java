e49d03151f1850ca7a4135fbdc8ccceef4c60702
Serhii <mishchenkoserhii7@gmail.com>
2023-06-16 22:41:59 +0300

    EcomedNpTelegramBotException added

---
+++ src/main/java/com/github/mishchuk7/ecomednpbot/v1/exception/EcomedNpTelegramBotException.java
@@ -0,0 +1,15 @@
+package com.github.mishchuk7.ecomednpbot.v1.exception;
+
+public class EcomedNpTelegramBotException extends RuntimeException{
+
+    public EcomedNpTelegramBotException() {
+    }
+
+    public EcomedNpTelegramBotException(String message) {
+        super(message);
+    }
+
+    public EcomedNpTelegramBotException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
